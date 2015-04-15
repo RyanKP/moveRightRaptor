f281afc9f32dde60aa2b05e3c9ceb35ec49324b0
RyanKP <thesuperbass@gmail.com>
2015-04-15 16:01:17 -0700

    Moving platforms
    fixed moving platform functionality. Separated JS from HTML and CSS

--- Platformer/movement.js
+++ Platformer/movement.js
@@ -1,172 +1,172 @@
 function movement(){
 	var pcTop = parseInt(pc.style.top);
-// HORIZONTAL MOVEMENT
+	// HORIZONTAL MOVEMENT
 
-				if(leftArrowDown){
-					pc.style.left = parseInt(pc.style.left) - 10 + 'px';
+	if(leftArrowDown){
+		pc.style.left = parseInt(pc.style.left) - 10 + 'px';
 
-					var sideHit = false;
-					for(var i=0; i<platforms.length; i++){
-						if( hittest(pc, platforms[i]) ) sideHit = true;
-					}
-					for (var i=0; i<movingPlatforms.length; i++){
-						if( hittest(pc, movingPlatforms[i]) ) sideHit = true;
-					}
-					pc.style.left = parseInt(pc.style.left) + 10 + 'px';
+		var sideHit = false;
+		for(var i=0; i<platforms.length; i++){
+			if( hittest(pc, platforms[i]) ) sideHit = true;
+		}
+		for (var i=0; i<hMPlatforms.length; i++){
+			if( hittest(pc, hMPlatforms[i]) ) sideHit = true;
+		}
+		pc.style.left = parseInt(pc.style.left) + 10 + 'px';
 
-					if( ! sideHit ){
-						platformDiv.style.left = parseInt(platformDiv.style.left) + 10 + 'px';
+		if( ! sideHit ){
+			platformDiv.style.left = parseInt(platformDiv.style.left) + 10 + 'px';
+		}
+	}
+	if(rightArrowDown){
+		
+		pc.style.left = parseInt(pc.style.left) + 10 + 'px';
+		document.getElementById('level').style.left = parseInt(document.getElementById('level').style.left - 10 + 'px')	
+
+		var sideHit = false;
+		for(var i=0; i<platforms.length; i++){
+			if( hittest(pc, platforms[i]) )sideHit = true;
+		}
+		for(var i=0; i<hMPlatforms.length; i++){
+			if( hittest(pc, hMPlatforms[i]) )sideHit = true;
+		}
+		pc.style.left = parseInt(pc.style.left) - 10 + 'px';
+		
+		if( ! sideHit ) {
+			platformDiv.style.left = parseInt(platformDiv.style.left) - 10 + 'px';
+		}
+
+	}
+
+	// VERTICAL MOVEMENT
+	if (fallSpeed>0){
+		pc.src = "princessFall.gif";
+	}
+
+
+	if (fallSpeed <= 500){
+		fallSpeed += GRAVITY;
+	}
+	else {
+		fallSpeed = 500;
+	}
+	pc.style.top = parseInt(pc.style.top) + fallSpeed + 'px';
+
+	for(var i=0; i<platforms.length; i++){
+		if( hittest(pc, platforms[i]) ){
+			if(fallSpeed<0){
+				pc.style.top = parseInt(platforms[i].style.top) + parseInt(platforms[i].style.height) + 'px';
+				//pc.style.top = pcDiv.style.top;
+				fallSpeed = -1;
+			}
+			else{
+				pc.style.top = parseInt(platforms[i].style.top) - parseInt(pc.style.height) + 'px';
+				//pc.style.top = pcDiv.style.top;
+				if(upArrowDown){ 
+					fallSpeed = -25;
+					if(parseInt(pc.style.top) < pcTop){
+						// document.getElementById('sndLand').currentTime = 0;
+						// document.getElementById('sndLand').volume = 0.1;
+						// document.getElementById('sndLand').play();
 					}
+				
+				} 
+				else{
+					fallSpeed = 0;
 				}
-				if(rightArrowDown){
-					
-					pc.style.left = parseInt(pc.style.left) + 10 + 'px';
-					document.getElementById('level').style.left = parseInt(document.getElementById('level').style.left - 10 + 'px')	
+			}
+			// break;
+		}
+	}
 
-					var sideHit = false;
-					for(var i=0; i<platforms.length; i++){
-						if( hittest(pc, platforms[i]) )sideHit = true;
-					}
-					for(var i=0; i<movingPlatforms.length; i++){
-						if( hittest(pc, movingPlatforms[i]) )sideHit = true;
-					}
-					pc.style.left = parseInt(pc.style.left) - 10 + 'px';
-					
-					if( ! sideHit ) {
-						platformDiv.style.left = parseInt(platformDiv.style.left) - 10 + 'px';
-					}
+	for(var i=0; i<hMPlatforms.length; i++){
+		if( hittest(pc, hMPlatforms[i]) ){
+			if(fallSpeed<0){
+				pc.style.top = parseInt(hMPlatforms[i].style.top) + parseInt(hMPlatforms[i].style.height) + 'px';
+				//pc.style.top = pcDiv.style.top;
+				fallSpeed = -1;
+			}
+			else{
+				pc.style.top = parseInt(hMPlatforms[i].style.top) - parseInt(pc.style.height) + 'px';
+				//pc.style.top = pcDiv.style.top;
+					if(hMPRight[i]){
+						var sideHit = false;
 
-				}
+						pc.style.left = parseInt(pc.style.left) - 5 + 'px';
+						
+						for(var j=0; j<platforms.length; j++){
+							if( hittest(pc, platforms[j]) ) sideHit = true;
+						}
+						for (var j=0; j<hMPlatforms.length; j++){
+							if( hittest(pc, hMPlatforms[j]) ) sideHit = true;
+						}
+						pc.style.left = parseInt(pc.style.left) + 5 + 'px';
 
-// VERTICAL MOVEMENT
-				if (fallSpeed>0){
-					pc.src = "princessFall.gif";
+						pc.style.left = parseInt(pc.style.left) + 5 + 'px';
+
+						
+						for(var j=0; j<platforms.length; j++){
+							if( hittest(pc, platforms[j]) ) sideHit = true;
+						}
+						for (var j=0; j<hMPlatforms.length; j++){
+							if( hittest(pc, hMPlatforms[j]) ) sideHit = true;
+						}
+						pc.style.left = parseInt(pc.style.left) - 5 + 'px';
+
+						if(! sideHit){
+							platformDiv.style.left = parseInt(platformDiv.style.left) - 5 + 'px';
+						}
+						else{
+							platformDiv.style.left = parseInt(platformDiv.style.left) + 0 + 'px';
+						}
+					}
+					else{
+
+						var sideHit = false;
+
+						pc.style.left = parseInt(pc.style.left) + 5 + 'px';
+
+						
+						for(var j=0; j<platforms.length; j++){
+							if( hittest(pc, platforms[j]) ) sideHit = true;
+						}
+						for (var j=0; j<hMPlatforms.length; j++){
+							if( hittest(pc, hMPlatforms[j]) ) sideHit = true;
+						}
+						pc.style.left = parseInt(pc.style.left) - 5 + 'px';
+
+						pc.style.left = parseInt(pc.style.left) - 5 + 'px';
+
+						for(var j=0; j<platforms.length; j++){
+							if( hittest(pc, platforms[j]) ) sideHit = true;
+						}
+						for (var j=0; j<hMPlatforms.length; j++){
+							if( hittest(pc, hMPlatforms[j]) ) sideHit = true;
+						}
+						pc.style.left = parseInt(pc.style.left) + 5 + 'px';
+
+						if(! sideHit){
+							platformDiv.style.left = parseInt(platformDiv.style.left) + 5 + 'px';
+						}
+						else{
+							platformDiv.style.left = parseInt(platformDiv.style.left) - 0 + 'px';
+						}
+					}
 				}
 				
 
-				if (fallSpeed <= 500){
-					fallSpeed += GRAVITY;
+				if(upArrowDown) {
+					fallSpeed = -25;
+					// document.getElementById('sndLand').currentTime = 0;
+					// document.getElementById('sndLand').volume = 0.1;
+					// document.getElementById('sndLand').play();
+				} 
+				else{
+					fallSpeed = 0;
 				}
-				else {
-					fallSpeed = 500;
-				}
-				pc.style.top = parseInt(pc.style.top) + fallSpeed + 'px';
-
-				for(var i=0; i<platforms.length; i++){
-					if( hittest(pc, platforms[i]) ){
-						if(fallSpeed<0){
-							pc.style.top = parseInt(platforms[i].style.top) + parseInt(platforms[i].style.height) + 'px';
-							//pc.style.top = pcDiv.style.top;
-							fallSpeed = -1;
-						}
-						else{
-							pc.style.top = parseInt(platforms[i].style.top) - parseInt(pc.style.height) + 'px';
-							//pc.style.top = pcDiv.style.top;
-							if(upArrowDown){ 
-								fallSpeed = -25;
-								if(parseInt(pc.style.top) < pcTop){
-									document.getElementById('sndLand').currentTime = 0;
-									document.getElementById('sndLand').volume = 0.1;
-									document.getElementById('sndLand').play();
-								}
-							
-							} 
-							else{
-								fallSpeed = 0;
-							}
-						}
-						// break;
-					}
-				}
-
-				for(var i=0; i<movingPlatforms.length; i++){
-					if( hittest(pc, movingPlatforms[i]) ){
-						if(fallSpeed<0){
-							pc.style.top = parseInt(movingPlatforms[i].style.top) + parseInt(movingPlatforms[i].style.height) + 'px';
-							//pc.style.top = pcDiv.style.top;
-							fallSpeed = -1;
-						}
-						else{
-							pc.style.top = parseInt(movingPlatforms[i].style.top) - parseInt(pc.style.height) + 'px';
-							//pc.style.top = pcDiv.style.top;
-							if(mPRight){
-								var sideHit = false;
-
-								pc.style.left = parseInt(pc.style.left) - 5 + 'px';
-								
-								for(var j=0; j<platforms.length; j++){
-									if( hittest(pc, platforms[j]) ) sideHit = true;
-								}
-								for (var j=0; j<movingPlatforms.length; j++){
-									if( hittest(pc, movingPlatforms[j]) ) sideHit = true;
-								}
-								pc.style.left = parseInt(pc.style.left) + 5 + 'px';
-
-								pc.style.left = parseInt(pc.style.left) + 5 + 'px';
-
-								
-								for(var j=0; j<platforms.length; j++){
-									if( hittest(pc, platforms[j]) ) sideHit = true;
-								}
-								for (var j=0; j<movingPlatforms.length; j++){
-									if( hittest(pc, movingPlatforms[j]) ) sideHit = true;
-								}
-								pc.style.left = parseInt(pc.style.left) - 5 + 'px';
-
-								if(! sideHit){
-									platformDiv.style.left = parseInt(platformDiv.style.left) - 5 + 'px';
-								}
-								else{
-									platformDiv.style.left = parseInt(platformDiv.style.left) + 0 + 'px';
-								}
-							}
-							if(mPLeft){
-
-								var sideHit = false;
-
-								pc.style.left = parseInt(pc.style.left) + 5 + 'px';
-
-								
-								for(var j=0; j<platforms.length; j++){
-									if( hittest(pc, platforms[j]) ) sideHit = true;
-								}
-								for (var j=0; j<movingPlatforms.length; j++){
-									if( hittest(pc, movingPlatforms[j]) ) sideHit = true;
-								}
-								pc.style.left = parseInt(pc.style.left) - 5 + 'px';
-
-								pc.style.left = parseInt(pc.style.left) - 5 + 'px';
-
-								for(var j=0; j<platforms.length; j++){
-									if( hittest(pc, platforms[j]) ) sideHit = true;
-								}
-								for (var j=0; j<movingPlatforms.length; j++){
-									if( hittest(pc, movingPlatforms[j]) ) sideHit = true;
-								}
-								pc.style.left = parseInt(pc.style.left) + 5 + 'px';
-
-								if(! sideHit){
-									platformDiv.style.left = parseInt(platformDiv.style.left) + 5 + 'px';
-								}
-								else{
-									platformDiv.style.left = parseInt(platformDiv.style.left) - 0 + 'px';
-								}
-							}
-
-							if(upArrowDown) {
-								fallSpeed = -25;
-								document.getElementById('sndLand').currentTime = 0;
-								document.getElementById('sndLand').volume = 0.1;
-								document.getElementById('sndLand').play();
-							} 
-							else{
-								fallSpeed = 0;
-							}
-						}
-					}
-					
-					
-				}
+			
+		}
+	}
 }
 
 			document.addEventListener('keydown', function(event){
@@ -183,54 +183,54 @@ function movement(){
 						clearInterval(gameTimer);
 						document.getElementById('pause').style.display = 'block';
 
-						if(!document.getElementById('music1').paused){
-							document.getElementById('music1').volume = 0;
-						}
-						if(!document.getElementById('music2').paused){
-							document.getElementById('music2').volume = 0;
-						}
-						if(!document.getElementById('music3').paused){
-							document.getElementById('music3').volume = 0;
-						}
-						if(!document.getElementById('sndLand').paused){
-							document.getElementById('sndLand').volume = 0;
-						}
-						if(!document.getElementById('sndDie').paused){
-							document.getElementById('sndDie').volume = 0;
-						}
-						if(!document.getElementById('get1').paused){
-							document.getElementById('get1').volume = 0;
-						}
-						if(!document.getElementById('get2').paused){
-							document.getElementById('get2').volume = 0;
-						}
-						if(!document.getElementById('get3').paused){
-							document.getElementById('get3').volume = 0;
-						}
-						if(document.getElementById('music1').volume==0){
-							document.getElementById('music1').pause();
-						}
-						if(document.getElementById('music2').volume==0){
-							document.getElementById('music2').pause();
-						}
-						if(document.getElementById('music3').volume==0){
-							document.getElementById('music3').pause();
-						}
-						if(document.getElementById('sndLand').volume==0){
-							document.getElementById('sndLand').pause();
-						}
-						if(document.getElementById('sndDie').volume==0){
-							document.getElementById('sndDie').pause();
-						}
-						if(document.getElementById('get1').volume==0){
-							document.getElementById('get1').pause();
-						}
-						if(document.getElementById('get2').volume==0){
-							document.getElementById('get2').pause();
-						}
-						if(document.getElementById('get3').volume==0){
-							document.getElementById('get3').pause();
-						}
+						// if(!document.getElementById('music1').paused){
+						// 	document.getElementById('music1').volume = 0;
+						// }
+						// if(!document.getElementById('music2').paused){
+						// 	document.getElementById('music2').volume = 0;
+						// }
+						// if(!document.getElementById('music3').paused){
+						// 	document.getElementById('music3').volume = 0;
+						// }
+						// if(!document.getElementById('sndLand').paused){
+						// 	document.getElementById('sndLand').volume = 0;
+						// }
+						// if(!document.getElementById('sndDie').paused){
+						// 	document.getElementById('sndDie').volume = 0;
+						// }
+						// if(!document.getElementById('get1').paused){
+						// 	document.getElementById('get1').volume = 0;
+						// }
+						// if(!document.getElementById('get2').paused){
+						// 	document.getElementById('get2').volume = 0;
+						// }
+						// if(!document.getElementById('get3').paused){
+						// 	document.getElementById('get3').volume = 0;
+						// }
+						// if(document.getElementById('music1').volume==0){
+						// 	document.getElementById('music1').pause();
+						// }
+						// if(document.getElementById('music2').volume==0){
+						// 	document.getElementById('music2').pause();
+						// }
+						// if(document.getElementById('music3').volume==0){
+						// 	document.getElementById('music3').pause();
+						// }
+						// if(document.getElementById('sndLand').volume==0){
+						// 	document.getElementById('sndLand').pause();
+						// }
+						// if(document.getElementById('sndDie').volume==0){
+						// 	document.getElementById('sndDie').pause();
+						// }
+						// if(document.getElementById('get1').volume==0){
+						// 	document.getElementById('get1').pause();
+						// }
+						// if(document.getElementById('get2').volume==0){
+						// 	document.getElementById('get2').pause();
+						// }
+						// if(document.getElementById('get3').volume==0){
+						// 	document.getElementById('get3').pause();
+						// }
 					}
 				}
 			});
